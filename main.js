0080cd79b2b430f29cfc03c4185d805ca3fb77d1
Ultimamaximus <82238915+Ultimamaximus@users.noreply.github.com>
2023-03-10 16:00:06 -0800

    Started a refactor to match BEM naming convention.

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
-const collapsibles = document.querySelectorAll(".collapsible");
-collapsibles.forEach((item) =>
-  item.addEventListener("click", function () {
-    this.classList.toggle("collapsible--expanded");
-  })
-);
+
+// Sends a confirmation email to user
+function newElement() {
+let inputValue = document.getElementById("email-input").value;
+if (!isNaN(inputValue) || inputValue === "") {
+  alert("Please input a number!");
+} else {
+  emailjs.send("Subscription_Service","Email_Subscription",{
+    email: inputValue
+    });
+  }
+}
